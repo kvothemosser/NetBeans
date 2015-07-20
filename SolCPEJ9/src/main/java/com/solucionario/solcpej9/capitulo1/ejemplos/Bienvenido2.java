1e44b8b0cb9e65f406389f0fd417bf9eaa800b47
kvothemosser <moises.tellez.tapia@gmail.com>
2015-07-20 13:38:32 -0500

    Segundo ejemplo de multiples lineas en un archivo

---
+++ SolCPEJ9/src/main/java/com/solucionario/solcpej9/capitulo1/ejemplos/Bienvenido2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.solucionario.solcpej9.capitulo1.ejemplos;
+
+/**
+ *
+ * @author mtapiat
+ */
+public class Bienvenido2 {
+    /**
+     * El Metodo main empieza la ejecucion de la aplicacion en Java.
+     * @param args 
+     */
+    public static void main(String[] args) {
+        System.out.print("Bienvenido a ");
+        System.out.println("la programacion en Java!");
+    }
+}
