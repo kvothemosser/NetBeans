f17b939778f4e744658616f00f868d76d070e567
kvothemosser <moises.tellez.tapia@gmail.com>
2015-07-20 13:13:28 -0500

    Primer ejemplo del libro.

---
+++ SolCPEJ9/src/main/java/com/solucionario/solcpej9/capitulo1/ejemplos/Bienvenido1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.solucionario.solcpej9.capitulo1.ejemplos;
+
+/**
+ * Programa para imprimir texto.
+ * @author mtapiat.
+ */
+public class Bienvenido1 {
+    /**
+     * El Metodo main empieza la ejecucion de la aplicacion en Java.
+     * @param args 
+     */
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a la programacion en Java!");
+    }
+}
