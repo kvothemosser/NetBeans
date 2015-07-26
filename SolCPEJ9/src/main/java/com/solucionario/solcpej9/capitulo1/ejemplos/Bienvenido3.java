ae9b544be3717e5350fce5f973e72955711dee2b
jorge <jorge@jorge-PC>
2015-07-25 21:24:19 -0500

    se hace clase de hola mundo

---
+++ SolCPEJ9/src/main/java/com/solucionario/solcpej9/capitulo1/ejemplos/Bienvenido3.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.solucionario.solcpej9.capitulo1.ejemplos;
+
+/**
+ *
+ * @author jorge
+ */
+public class Bienvenido3 {
+    
+   //el metodo main empieza la ejecucion de la aplicacion en java 
+    public static void main ( String[] args ){
+        System.out.println("Bienvenido\na\nla programacion\nen Java!");
+        
+    }
+}
